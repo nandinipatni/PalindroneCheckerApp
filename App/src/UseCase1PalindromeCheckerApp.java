8dcb155fffdbb75d0b8937a87e056cbc1bbf81a6
nandinipatni <nandinipatnijpr@gmail.com>
2026-02-19 09:45:10 +0530

    Nandini Added UseCase1

---
+++ App/src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Application Name
+        System.out.println("==================================");
+        System.out.println("      Palindrome Checker App      ");
+        System.out.println("==================================");
+
+        // Application Version
+        System.out.println("Version: 1.0");
+
+        // Welcome Message
+        System.out.println("Welcome to the Palindrome Checker Application!");
+
+        System.out.println("Application Started Successfully.");
+    }
+}
