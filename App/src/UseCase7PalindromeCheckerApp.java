53b88d25b0f357707e0b1ab55ed414d2217c6b81
Revani-prog <rev123wwn@gmail.com>
2026-03-06 08:37:46 +0530

    Nandini Added UseCase7

---
+++ App/src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,27 @@
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        char[] arr = word.toCharArray();
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < arr.length / 2; i++) {
+
+            if (arr[i] != arr[arr.length - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+
+    }
+}
